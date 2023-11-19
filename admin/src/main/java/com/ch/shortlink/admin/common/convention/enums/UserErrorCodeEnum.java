b5d9df73d17199615bbe6049828faa940649c987
caohui <1518935897@qq.com>
2023-11-19 22:27:09 +0800

    feature: 开发短链接规约功能

---
+++ admin/src/main/java/com/ch/shortlink/admin/common/convention/enums/UserErrorCodeEnum.java
@@ -0,0 +1,32 @@
+package com.ch.shortlink.admin.common.convention.enums;
+
+import com.ch.shortlink.admin.common.convention.errorcode.IErrorCode;
+
+/**
+ * @Author hui cao
+ * @Description:
+ */
+public enum UserErrorCodeEnum implements IErrorCode {
+    USER_NULL("B000200", "用户记录不存在"),
+
+    USER_EXIST("B000201", "用户记录已存在");
+
+    private final String code;
+
+    private final String message;
+
+    UserErrorCodeEnum(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    @Override
+    public String code() {
+        return code;
+    }
+
+    @Override
+    public String message() {
+        return message;
+    }
+}
---
+++ admin/src/main/java/com/ch/shortlink/admin/common/convention/errorcode/IErrorCode.java
@@ -0,0 +1,18 @@
+package com.ch.shortlink.admin.common.convention.errorcode;
+
+/**
+ * @Author hui cao
+ * @Description: 平台错误码
+ */
+public interface IErrorCode {
+
+    /**
+     * 错误码
+     */
+    String code();
+
+    /**
+     * 错误信息
+     */
+    String message();
+}
