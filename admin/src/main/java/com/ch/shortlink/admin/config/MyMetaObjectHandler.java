0e1735f2ca237ede928a16686b1ae4ddd4a1dcdb
caohui <1518935897@qq.com>
2023-11-20 14:20:08 +0800

    feature: 开发短链接后管系统通用持久层实体对象属性填充

---
+++ admin/src/main/java/com/ch/shortlink/admin/config/MyMetaObjectHandler.java
@@ -0,0 +1,28 @@
+package com.ch.shortlink.admin.config;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+/**
+ * @Author hui cao
+ * @Description:
+ */
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.strictInsertFill(metaObject, "createTime", Date::new, Date.class);
+        this.strictInsertFill(metaObject, "updateTime", Date::new, Date.class);
+        this.strictInsertFill(metaObject, "delFlag", () -> 0, Integer.class);
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.strictInsertFill(metaObject, "updateTime", LocalDateTime::now, LocalDateTime.class);
+    }
+}
