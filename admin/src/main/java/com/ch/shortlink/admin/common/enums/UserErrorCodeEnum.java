df9afe8e0d540f67fe81e296185e22d2277d8559
caohui <1518935897@qq.com>
2023-11-21 13:13:43 +0800

    refactor: 增加token相关错误码

--- admin/src/main/java/com/ch/shortlink/admin/common/enums/UserErrorCodeEnum.java
+++ admin/src/main/java/com/ch/shortlink/admin/common/enums/UserErrorCodeEnum.java
@@ -7,6 +7,8 @@ import com.ch.shortlink.admin.common.convention.errorcode.IErrorCode;
  * @Description:
  */
 public enum UserErrorCodeEnum implements IErrorCode {
+
+    USER_TOKEN_FAIL("A000200", "用户token验证失败"),
     USER_NULL("B000200", "用户记录不存在"),
 
     USER_NAME_EXIST("B000201", "用户名已存在"),
