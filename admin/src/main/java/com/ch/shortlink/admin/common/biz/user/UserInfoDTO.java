e22f1e31ccd2bf39746dca2b95564299986d3230
caohui <1518935897@qq.com>
2023-11-21 13:15:35 +0800

    feature: 开发封装用户上下文

---
+++ admin/src/main/java/com/ch/shortlink/admin/common/biz/user/UserInfoDTO.java
@@ -0,0 +1,35 @@
+package com.ch.shortlink.admin.common.biz.user;
+
+import com.alibaba.fastjson2.annotation.JSONField;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @Author hui cao
+ * @Description: 用户信息实体
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserInfoDTO {
+
+    /**
+     * 用户 ID
+     */
+    @JSONField(name = "id")
+    private String userId;
+
+    /**
+     * 用户名
+     */
+    private String username;
+
+    /**
+     * 真实姓名
+     */
+    private String realName;
+
+}
