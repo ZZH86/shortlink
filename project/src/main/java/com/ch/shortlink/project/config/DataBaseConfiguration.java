ca0fa6bcf95b79d6c44155e90be0ba8ad522e764
caohui <1518935897@qq.com>
2023-11-24 22:11:02 +0800

    feature: 开发MySQL数据库分页插件

---
+++ project/src/main/java/com/ch/shortlink/project/config/DataBaseConfiguration.java
@@ -0,0 +1,25 @@
+package com.ch.shortlink.project.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author hui cao
+ * @Description: 分页插件
+ */
+@Configuration
+public class DataBaseConfiguration {
+
+    /**
+     * MybatisPlus 分页插件
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor(){
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
