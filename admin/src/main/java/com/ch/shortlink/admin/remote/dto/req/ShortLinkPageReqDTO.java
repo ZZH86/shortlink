66ad005edb7735ec97b11b3dc57f28fe6d430064
caohui <1518935897@qq.com>
2023-11-25 12:49:51 +0800

    refactor: 优化短链接分页返回响应对象

--- admin/src/main/java/com/ch/shortlink/admin/remote/dto/req/ShortLinkPageReqDTO.java
+++ admin/src/main/java/com/ch/shortlink/admin/remote/dto/req/ShortLinkPageReqDTO.java
@@ -1,7 +1,6 @@
 package com.ch.shortlink.admin.remote.dto.req;
 
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import com.ch.shortlink.project.dao.entity.ShortLinkDO;
 import lombok.Data;
 
 /**
@@ -9,7 +8,7 @@ import lombok.Data;
  * @Description: 短链接分页请求参数
  */
 @Data
-public class ShortLinkPageReqDTO extends Page<ShortLinkDO> {
+public class ShortLinkPageReqDTO extends Page {
 
     /**
      * 分组标识
