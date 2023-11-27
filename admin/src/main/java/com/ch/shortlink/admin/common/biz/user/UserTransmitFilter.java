79f27e1037b4c1177d54317fb4d6dbe5bc65adc0
caohui <1518935897@qq.com>
2023-11-27 15:56:22 +0800

    refactor: 优化抛出异常

--- admin/src/main/java/com/ch/shortlink/admin/common/biz/user/UserTransmitFilter.java
+++ admin/src/main/java/com/ch/shortlink/admin/common/biz/user/UserTransmitFilter.java
@@ -34,8 +34,7 @@ public class UserTransmitFilter implements Filter {
 
     @SneakyThrows
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
-            throws IOException, ServletException {
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) {
         HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
         String requestURI = httpServletRequest.getRequestURI();
         if (!IGNORE_URI.contains(requestURI)) {
