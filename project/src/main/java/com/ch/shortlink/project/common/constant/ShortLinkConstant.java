e41736d95bb4da8c7816f379aeacaad20ec32835
caohui <1518935897@qq.com>
2023-11-27 13:30:57 +0800

    feature: 定义短链接常量类过期时间

---
+++ project/src/main/java/com/ch/shortlink/project/common/constant/ShortLinkConstant.java
@@ -0,0 +1,13 @@
+package com.ch.shortlink.project.common.constant;
+
+/**
+ * @Author hui cao
+ * @Description: 短链接常量类
+ */
+public class ShortLinkConstant {
+
+    /**
+     * 永久短链接默认缓存有效时间（一个月）
+     */
+    public static final long DEFAULT_CACHE_VALID_TIME = 2526560000L;
+}
