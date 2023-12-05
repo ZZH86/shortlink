15147b2f678cc145636be0869753fdbaa61e16a4
caohui <1518935897@qq.com>
2023-12-05 21:54:19 +0800

    feature: 开发短链接监控今日统计功能

---
+++ project/src/main/java/com/ch/shortlink/project/dao/entity/LinkStatsTodayDO.java
@@ -0,0 +1,57 @@
+package com.ch.shortlink.project.dao.entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.ch.shortlink.project.common.database.BaseDO;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+/**
+ * @Author hui cao
+ * @Description: 短链接今日统计实体
+ */
+@TableName("t_link_stats_today")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LinkStatsTodayDO extends BaseDO {
+
+    /**
+     * id
+     */
+    private Long id;
+
+    /**
+     * 分组标识
+     */
+    private String gid;
+
+    /**
+     * 短链接
+     */
+    private String fullShortUrl;
+
+    /**
+     * 日期
+     */
+    private Date date;
+
+    /**
+     * 今日pv
+     */
+    private Integer todayPv;
+
+    /**
+     * 今日uv
+     */
+    private Integer todayUv;
+
+    /**
+     * 今日ip数
+     */
+    private Integer todayUip;
+}
