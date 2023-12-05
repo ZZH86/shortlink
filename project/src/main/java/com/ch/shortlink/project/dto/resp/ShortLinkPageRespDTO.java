14c49696035fb06e7b22e32ec68442dc672acd5d
caohui <1518935897@qq.com>
2023-12-05 21:52:49 +0800

    feature: 增加短链接监控分页返回响应

--- project/src/main/java/com/ch/shortlink/project/dto/resp/ShortLinkPageRespDTO.java
+++ project/src/main/java/com/ch/shortlink/project/dto/resp/ShortLinkPageRespDTO.java
@@ -68,4 +68,34 @@ public class ShortLinkPageRespDTO {
      */
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date createTime;
+
+    /**
+     * 历史PV
+     */
+    private Integer totalPv;
+
+    /**
+     * 今日PV
+     */
+    private Integer todayPv;
+
+    /**
+     * 历史UV
+     */
+    private Integer totalUv;
+
+    /**
+     * 历史UIP
+     */
+    private Integer totalUip;
+
+    /**
+     * 今日UV
+     */
+    private Integer todayUv;
+
+    /**
+     * 今日UIP
+     */
+    private Integer todayUip;
 }
