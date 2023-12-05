773a68da079d2a99eccc58d147de4976feaacc44
caohui <1518935897@qq.com>
2023-12-05 21:38:36 +0800

    redactor: 增加监控统计实体参数

--- project/src/main/java/com/ch/shortlink/project/dao/entity/ShortLinkDO.java
+++ project/src/main/java/com/ch/shortlink/project/dao/entity/ShortLinkDO.java
@@ -86,4 +86,37 @@ public class ShortLinkDO extends BaseDO {
      */
     @TableField("`describe`")
     private String describe;
+
+    /**
+     * 历史PV
+     */
+    private Integer totalPv;
+
+    /**
+     * 历史UV
+     */
+    private Integer totalUv;
+
+    /**
+     * 历史UIP
+     */
+    private Integer totalUip;
+
+    /**
+     * 今日PV
+     */
+    @TableField(exist = false)
+    private Integer todayPv;
+
+    /**
+     * 今日UV
+     */
+    @TableField(exist = false)
+    private Integer todayUv;
+
+    /**
+     * 今日UIP
+     */
+    @TableField(exist = false)
+    private Integer todayUip;
 }
