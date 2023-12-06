c77c967d206d2507e4d2ec71ddc92d977daf05f2
caohui <1518935897@qq.com>
2023-12-06 16:28:53 +0800

    fix: 修改请求过滤忽略uri

--- admin/src/main/java/com/ch/shortlink/admin/common/biz/user/UserTransmitFilter.java
+++ admin/src/main/java/com/ch/shortlink/admin/common/biz/user/UserTransmitFilter.java
@@ -28,8 +28,8 @@ public class UserTransmitFilter implements Filter {
     private final StringRedisTemplate stringRedisTemplate;
 
     private static final List<String> IGNORE_URI = Lists.newArrayList(
-            "/api/short-link/v1/user/login",
-            "/api/short-link/v1/user/has-username"
+            "/api/short-link/admin/v1/user/login",
+            "/api/short-link/admin/v1/user/has-username"
     );
 
     @SneakyThrows
