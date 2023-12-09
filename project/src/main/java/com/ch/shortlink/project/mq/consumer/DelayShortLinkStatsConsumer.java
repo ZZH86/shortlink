7a4677b0c3ee35bcb1da3e95025ffbbadf9bf72d
caohui <1518935897@qq.com>
2023-12-09 16:12:17 +0800

    feature: 开发延迟记录短链接统计组件

---
+++ project/src/main/java/com/ch/shortlink/project/mq/consumer/DelayShortLinkStatsConsumer.java
@@ -0,0 +1,57 @@
+package com.ch.shortlink.project.mq.consumer;
+
+import com.ch.shortlink.project.dto.biz.ShortLinkStatsRecordDTO;
+import com.ch.shortlink.project.service.ShortLinkService;
+import lombok.RequiredArgsConstructor;
+import org.redisson.api.RBlockingDeque;
+import org.redisson.api.RDelayedQueue;
+import org.redisson.api.RedissonClient;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.LockSupport;
+
+import static com.ch.shortlink.project.common.constant.RedisKeyConstant.DELAY_QUEUE_STATS_KEY;
+
+/**
+ * @Author hui cao
+ * @Description: 延迟记录短链接统计组件
+ */
+@Component
+@RequiredArgsConstructor
+public class DelayShortLinkStatsConsumer implements InitializingBean {
+
+    private final RedissonClient redissonClient;
+    private final ShortLinkService shortLinkService;
+
+    public void onMessage() {
+        Executors.newSingleThreadExecutor(
+                        runnable -> {
+                            Thread thread = new Thread(runnable);
+                            thread.setName("delay_short-link_stats_consumer");
+                            thread.setDaemon(Boolean.TRUE);
+                            return thread;
+                        })
+                .execute(() -> {
+                    RBlockingDeque<ShortLinkStatsRecordDTO> blockingDeque = redissonClient.getBlockingDeque(DELAY_QUEUE_STATS_KEY);
+                    RDelayedQueue<ShortLinkStatsRecordDTO> delayedQueue = redissonClient.getDelayedQueue(blockingDeque);
+                    for (; ; ) {
+                        try {
+                            ShortLinkStatsRecordDTO statsRecord = delayedQueue.poll();
+                            if (statsRecord != null) {
+                                shortLinkService.shortLinkStats(null, null, statsRecord);
+                                continue;
+                            }
+                            LockSupport.parkUntil(500);
+                        } catch (Throwable ignored) {
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        onMessage();
+    }
+}
---
+++ project/src/main/java/com/ch/shortlink/project/mq/producer/DelayShortLinkStatsProducer.java
@@ -0,0 +1,34 @@
+package com.ch.shortlink.project.mq.producer;
+
+import com.ch.shortlink.project.dto.biz.ShortLinkStatsRecordDTO;
+import lombok.RequiredArgsConstructor;
+import org.redisson.api.RBlockingDeque;
+import org.redisson.api.RDelayedQueue;
+import org.redisson.api.RedissonClient;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+import static com.ch.shortlink.project.common.constant.RedisKeyConstant.DELAY_QUEUE_STATS_KEY;
+
+/**
+ * @Author hui cao
+ * @Description: 延迟消费短链接统计发送者
+ */
+@Component
+@RequiredArgsConstructor
+public class DelayShortLinkStatsProducer {
+
+    private final RedissonClient redissonClient;
+
+    /**
+     * 发送延迟消费短链接统计
+     *
+     * @param statsRecord 短链接统计实体参数
+     */
+    public void send(ShortLinkStatsRecordDTO statsRecord) {
+        RBlockingDeque<ShortLinkStatsRecordDTO> blockingDeque = redissonClient.getBlockingDeque(DELAY_QUEUE_STATS_KEY);
+        RDelayedQueue<ShortLinkStatsRecordDTO> delayedQueue = redissonClient.getDelayedQueue(blockingDeque);
+        delayedQueue.offer(statsRecord, 5, TimeUnit.SECONDS);
+    }
+}
