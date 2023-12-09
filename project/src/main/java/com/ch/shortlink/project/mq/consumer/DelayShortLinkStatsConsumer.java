e2b37abf262499346926ef902b9825160886e1d1
caohui <1518935897@qq.com>
2023-12-09 20:50:23 +0800

    docs: 消息队列消费者相关注释

--- project/src/main/java/com/ch/shortlink/project/mq/consumer/DelayShortLinkStatsConsumer.java
+++ project/src/main/java/com/ch/shortlink/project/mq/consumer/DelayShortLinkStatsConsumer.java
@@ -20,7 +20,7 @@ import static com.ch.shortlink.project.common.constant.RedisKeyConstant.DELAY_QU
  */
 @Component
 @RequiredArgsConstructor
-public class DelayShortLinkStatsConsumer implements InitializingBean {
+public class DelayShortLinkStatsConsumer implements InitializingBean {  // 启动阶段初始化
 
     private final RedissonClient redissonClient;
     private final ShortLinkService shortLinkService;
@@ -34,15 +34,18 @@ public class DelayShortLinkStatsConsumer implements InitializingBean {
                             return thread;
                         })
                 .execute(() -> {
+                    // 获取短链接延迟队列
                     RBlockingDeque<ShortLinkStatsRecordDTO> blockingDeque = redissonClient.getBlockingDeque(DELAY_QUEUE_STATS_KEY);
                     RDelayedQueue<ShortLinkStatsRecordDTO> delayedQueue = redissonClient.getDelayedQueue(blockingDeque);
-                    for (; ; ) {
+                    while (true) {
                         try {
                             ShortLinkStatsRecordDTO statsRecord = delayedQueue.poll();
                             if (statsRecord != null) {
+                                // 500 毫秒后有消息就进行消费
                                 shortLinkService.shortLinkStats(null, null, statsRecord);
                                 continue;
                             }
+                            // 出于线程调度目的禁用当前线程，直到指定的截止日期，除非许可可用。
                             LockSupport.parkUntil(500);
                         } catch (Throwable ignored) {
                         }
