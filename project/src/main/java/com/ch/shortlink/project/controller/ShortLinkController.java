550613604291bcde29c103dbd78e0afb4909ce5c
caohui <1518935897@qq.com>
2023-12-09 17:52:57 +0800

    docs: 添加短链接跳转接口相关注释

--- project/src/main/java/com/ch/shortlink/project/controller/ShortLinkController.java
+++ project/src/main/java/com/ch/shortlink/project/controller/ShortLinkController.java
@@ -26,6 +26,13 @@ import java.util.List;
 public class ShortLinkController {
 
     private final ShortLinkService shortLinkService;
+
+    /**
+     * 短链接跳转
+     * @param shortUri 短链接码
+     * @param request 请求
+     * @param response 响应
+     */
     @GetMapping("/{short-uri}")
     public void restoreUrl(@PathVariable("short-uri") String shortUri, ServletRequest request, ServletResponse response){
         shortLinkService.restoreUri(shortUri, request, response);
