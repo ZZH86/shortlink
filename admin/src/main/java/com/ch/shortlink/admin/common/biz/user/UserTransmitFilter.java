e4e426c5c38f1a54e8b815445d24ac2c5600ad29
caohui <1518935897@qq.com>
2023-12-09 16:11:25 +0800

    fix: 修改过滤器相关api

--- admin/src/main/java/com/ch/shortlink/admin/common/biz/user/UserTransmitFilter.java
+++ admin/src/main/java/com/ch/shortlink/admin/common/biz/user/UserTransmitFilter.java
@@ -39,7 +39,7 @@ public class UserTransmitFilter implements Filter {
         String requestURI = httpServletRequest.getRequestURI();
         if (!IGNORE_URI.contains(requestURI)) {
             String method = httpServletRequest.getMethod();
-            if (!(Objects.equals(requestURI, "/api/short-link/v1/user") && Objects.equals(method, "POST"))) {
+            if (!(Objects.equals(requestURI, "/api/short-link/admin/v1/user") && Objects.equals(method, "POST"))) {
                 String username = httpServletRequest.getHeader("username");
                 String token = httpServletRequest.getHeader("token");
                 if (!StrUtil.isAllNotBlank(username, token)) {
