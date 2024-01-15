c904635e6f997d8855f8773a818762a3302aec46
caohui <1518935897@qq.com>
2024-01-15 16:21:33 +0800

    feature:  短链接监控消费者幂等逻辑处理

--- project/src/main/java/com/ch/shortlink/project/mq/consumer/DelayShortLinkStatsMQConsumer.java
+++ project/src/main/java/com/ch/shortlink/project/mq/consumer/DelayShortLinkStatsMQConsumer.java
@@ -2,8 +2,10 @@ package com.ch.shortlink.project.mq.consumer;
 
 import com.alibaba.fastjson.JSON;
 import com.ch.shortlink.project.common.constant.RocketMQConstant;
+import com.ch.shortlink.project.common.convention.exception.ServiceException;
 import com.ch.shortlink.project.dto.biz.ShortLinkStatsRecordDTO;
 import com.ch.shortlink.project.mq.domain.MessageWrapper;
+import com.ch.shortlink.project.mq.idempotent.MessageQueueIdempotentHandler;
 import com.ch.shortlink.project.service.ShortLinkService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -26,17 +28,31 @@ import org.springframework.stereotype.Component;
 public class DelayShortLinkStatsMQConsumer implements RocketMQListener<MessageWrapper<ShortLinkStatsRecordDTO>> {
 
     private final ShortLinkService shortLinkService;
+    private final MessageQueueIdempotentHandler messageQueueIdempotentHandler;
 
     @Override
     public void onMessage(MessageWrapper<ShortLinkStatsRecordDTO> message) {
-        log.info("[延迟访问统计Consumer] 开始消费：{}", JSON.toJSONString(message));
+        // 获取唯一标识
+        String uuid = message.getUuid();
+
+        // 消息被消费
+        if(!messageQueueIdempotentHandler.isMessageProcessed(uuid)){
+            if(messageQueueIdempotentHandler.isAccomplish(uuid)){
+                return;
+            }
+            throw new ServiceException("消息未消费完成，需要消息队列重试");
+        }
+
+        // 消息未被消费
         ShortLinkStatsRecordDTO shortLinkStatsRecordDTO = message.getMessage();
         try{
+            log.info("[延迟访问统计Consumer] 开始消费：{}", JSON.toJSONString(message));
             shortLinkService.shortLinkStats(null,null,shortLinkStatsRecordDTO);
         }catch (Throwable ex){
             log.error("[延迟访问统计Consumer] 短链接：{} 访问统计服务失败", shortLinkStatsRecordDTO.getFullShortUrl(), ex);
-            throw ex;
+            messageQueueIdempotentHandler.delMessageProcessed(uuid);
         }
+        messageQueueIdempotentHandler.setAccomplish(uuid);
 
     }
 }
--- project/src/main/java/com/ch/shortlink/project/mq/consumer/ShortLinkStatsMQConsumer.java
+++ project/src/main/java/com/ch/shortlink/project/mq/consumer/ShortLinkStatsMQConsumer.java
@@ -2,8 +2,10 @@ package com.ch.shortlink.project.mq.consumer;
 
 import com.alibaba.fastjson.JSON;
 import com.ch.shortlink.project.common.constant.RocketMQConstant;
+import com.ch.shortlink.project.common.convention.exception.ServiceException;
 import com.ch.shortlink.project.dto.biz.ShortLinkStatsRecordDTO;
 import com.ch.shortlink.project.mq.domain.MessageWrapper;
+import com.ch.shortlink.project.mq.idempotent.MessageQueueIdempotentHandler;
 import com.ch.shortlink.project.service.ShortLinkService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -26,17 +28,34 @@ import org.springframework.stereotype.Component;
 public class ShortLinkStatsMQConsumer implements RocketMQListener<MessageWrapper<ShortLinkStatsRecordDTO>> {
 
     private final ShortLinkService shortLinkService;
+    private final MessageQueueIdempotentHandler messageQueueIdempotentHandler;
 
     @Override
     public void onMessage(MessageWrapper<ShortLinkStatsRecordDTO> message) {
-        log.info("[访问统计Consumer] 开始消费：{}", JSON.toJSONString(message));
-        ShortLinkStatsRecordDTO shortLinkStatsRecordDTO = message.getMessage();
-        try{
-            shortLinkService.shortLinkStats(null,null,shortLinkStatsRecordDTO);
-        }catch (Throwable ex){
-            log.error("[访问统计Consumer] 短链接：{} 访问统计服务失败", shortLinkStatsRecordDTO.getFullShortUrl(), ex);
-            throw ex;
+
+        String id = message.getUuid();
+
+        // 如果被消费过
+        if (!messageQueueIdempotentHandler.isMessageProcessed(id)) {
+            // 判断当前的这个消息流程是否执行完成
+            if (messageQueueIdempotentHandler.isAccomplish(id)) {
+                return;
+            }
+            throw new ServiceException("消息未完成流程，需要消息队列重试");
         }
 
+        // 没有被消费过
+        try {
+            log.info("[访问统计Consumer] 开始消费：{}", JSON.toJSONString(message));
+            ShortLinkStatsRecordDTO shortLinkStatsRecordDTO = message.getMessage();
+            shortLinkService.shortLinkStats(null,null,shortLinkStatsRecordDTO);
+        } catch (Throwable ex) {
+            // 某情况宕机了
+            // 如果消息处理遇到异常情况，删除幂等标识
+            messageQueueIdempotentHandler.delMessageProcessed(id);
+            log.error("记录短链接监控消费异常", ex);
+            throw new ServiceException("消息未完成流程，需要消息队列重试");
+        }
+        messageQueueIdempotentHandler.setAccomplish(id);
     }
 }
